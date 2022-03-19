69a7c790d744e96a008d533287bae911a0342e85
Felipe Rosa <felipe.squall@gmail.com>
2022-03-19 14:02:16 -0300

    Instalação do Mongoose e criação dos Models checklist e Task

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const checkListRouter = require('./src/routes/checklist')
+const checkListRouter = require('./src/routes/checklist');
+require('./config/databse');
 
 const app = express();
 app.use(express.json());
