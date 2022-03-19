fd72f5176ab973aafaf3f1cad762f0f76f142ce2
Felipe Rosa <felipe.squall@gmail.com>
2022-03-19 15:15:32 -0300

    Realizando CRUD no MongoDB

--- src/models/checklist.js
+++ src/models/checklist.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
 const checklistSchema = mongoose.Schema({
-    name: {type: String, required: true},
-    tasks: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Task'}]
+  name: {type: String, required: true},
+  tasks: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Task'
+  }]
 })
 
 module.exports = mongoose.model('Checklist', checklistSchema);
