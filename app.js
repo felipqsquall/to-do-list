c5140fb20975bae8708b01a0f4bec158f2ee038f
Felipe Rosa <felipe.squall@gmail.com>
2022-03-21 14:31:31 -0300

    Instalação Method-Override

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const methodOverride = require('method-override');
 
 const checkListRouter = require('./src/routes/checklist');
 const rootRouter = require('./src/routes/index');
@@ -11,6 +12,7 @@ require('./config/databse');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
